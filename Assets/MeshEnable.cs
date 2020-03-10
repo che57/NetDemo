a380eeff0cb7e0368c23bb228635d0b998eb5dac
che57 <35236747+che57@users.noreply.github.com>
2020-03-09 20:37:35 -0400

    fix other players display bug

--- Assets/MeshEnable.cs
+++ Assets/MeshEnable.cs
@@ -7,12 +7,11 @@ public class MeshEnable : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    this.gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
