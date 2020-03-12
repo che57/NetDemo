45726cd88b47a93e9dfe7a58660971131f0c9bc8
che57 <che57@uwo.ca>
2020-03-12 19:47:42 -0400

    fixed hand mesh render disable problem

--- Assets/Scripts/LabSetupController.cs
+++ Assets/Scripts/LabSetupController.cs
@@ -69,7 +69,6 @@ public class LabSetupController : MonoBehaviourPunCallbacks
                 otherPlayers.Add(otherPlayer);
             }
         }
-
     }
 
     void UpdateOtherPlayersPosition()
